42313c779d3148c86c4ad9208433e9290da0bc84
Olaf Hauk <olaf.hauk@mrc-cbu.cam.ac.uk>
2017-02-23 18:01:29 +0000

    facilitated switching between correlation and regression

--- plot_two_vectors.py
+++ plot_two_vectors.py
@@ -25,6 +25,7 @@ from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as Navigatio
 from matplotlib.figure import Figure
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+plot_colors = ['b', 'g', 'r', 'c', 'm', 'k', 'w']
 
 class AppForm(QMainWindow):
     def __init__(self, parent=None):
@@ -59,15 +60,11 @@ class AppForm(QMainWindow):
         if (text=='Vectors'):
             self.display_option = 0
         elif (text=='Regression'):
-            if hasattr(self, 'functext'): # start afresh if necessary
-                delattr(self, 'functext')
             self.display_option = 1
         elif (text=='Correlation'):
-            if hasattr(self, 'functext'): # start afresh if necessary
-                delattr(self, 'functext')
-            self.create_main_frame()
             self.display_option = 2
         
+        self.create_main_frame()
         self.on_draw()
     
     # def on_about(self):
@@ -104,23 +101,14 @@ class AppForm(QMainWindow):
 
     ## new method
     def curs_pos(self, event):
-        """ Print cursor position in canvas
+        """ get cursor position in canvas
         """
         # cursor position
         x = np.array([event.xdata, event.ydata])
         self.data_mouse = [0, 0, x[0], x[1]]
-        # coordinates from text box, shifted to origin
-        xt = [self.X[2], self.X[3]]
+        # coordinates from text box, shifted to origin        
         print "Cursor pos", x[0], x[1]
-        print "Text pos", xt[0], xt[1]
-        if (x[0] !=None and x[1] != None):
-            corr = np.dot(x,xt) / np.sqrt(np.dot(x,x)*np.dot(xt,xt))
-            angle = np.arccos( corr )
-            print "Correlation", corr
-            print "Angle: ", angle, " rad; ", 180*angle/np.pi, " deg"
-            
-            self.on_draw()
-            # print QCursor().cursor.pos()
+        self.on_draw()
 
     
     def on_draw(self):
@@ -129,6 +117,8 @@ class AppForm(QMainWindow):
         # str = unicode(self.textbox.text())
         # # self.data = map(int, str.split())
         # self.data = map(float, str.split())
+
+        print "Draw"
         
         # x = range(len(self.data))
 
@@ -158,11 +148,14 @@ class AppForm(QMainWindow):
 
         x = np.arange(self.x_lim[0],self.x_lim[1],self.x_lim[2]) # x-axis for plots
         x = x * self.slider_scale.value()/10. # scale x-axis with slider value
+        n = len(x) # for use in interactive displays
 
         ## display VECTORS
         if self.display_option==0:
 
-            ## Vector Plot        
+            ## Vector Plot
+            if self.data_mouse==[]:
+                self.data_mouse = [0,0,0.9,0]
             X = [ np.array([0,0,0,0.9]), np.array(self.data_mouse) ]            
             scale_fac = self.slider_scale.value() / 10.
             X[0] = X[0] * scale_fac # rescale; mouse coordinates are already scaled
@@ -174,7 +167,17 @@ class AppForm(QMainWindow):
                     self.axes.set_xlim(self.x_lim[0]*scale_fac, self.x_lim[1]*scale_fac)
                     self.axes.set_ylim(self.x_lim[0]*scale_fac, self.x_lim[1]*scale_fac)
             self.X = X[0] # keep reference vector
-
+                    
+            # angle between vectors
+            corr = np.dot(X[0],X[1]) / np.sqrt(np.dot(X[0],X[0])*np.dot(X[1],X[1]))
+            angle = np.arccos( corr )
+            disp_text = "  %.2f rad / %.2f deg, r = %.2f" % (angle, 180*angle/np.pi, corr)
+            self.axes.text(x=self.x_lim[0]*scale_fac,y=-0.9*self.x_lim[1]*scale_fac,s=disp_text)
+            # myarc = matplotlib.patches.Arc(xy=(0,0), width=1, height=1, angle=180, theta1=0, theta2=360)
+            # self.axes.add_patch(myarc)
+            print "Correlation", corr
+            print "Angle: ", angle, " rad; ", 180*angle/np.pi, " deg"
+                
         ## display REGRESSION
         elif self.display_option==1: # regression option chosen
             # textbox 2 (function execution)
@@ -188,17 +191,18 @@ class AppForm(QMainWindow):
             if len(text_lim)==2:
                 self.x_lim.append(0.01)  
 
-            if hasattr(self, 'functext'):
+            if hasattr(self, 'functext'):                
                 legend = []
                 ylist = [] # list of results
                 for ff in self.functext:
-                    str = unicode(ff.text())
-                    if (str != ''):
-                        y = np.array( eval(str) )
+                    txt_str = unicode(ff.text())
+                    print txt_str
+                    if (txt_str != ''):
+                        y = np.array( eval(txt_str) )
                         self.axes.plot(x,y)
                         self.axes.set_xlim(x[0],x[-1])
                         ylist.append(y)
-                        legend.append(str)
+                        legend.append(txt_str)
                 
                 if len(ylist)>1:
                     ymat = np.array(ylist)
@@ -247,9 +251,11 @@ class AppForm(QMainWindow):
             x = x * self.slider_scale.value()/10. # scale x-axis with slider value
             y = y * self.slider_scale.value()/10. # scale x-axis with slider value
 
+            n = len(x) # for use in interactive display
+
             text_list = [] # text strings to evaluate as functions for scatter plots
-            if (hasattr(self, 'functext')):
-                for ff in self.functext:
+            if (hasattr(self, 'corrtext')):
+                for ff in self.corrtext:
                     if len(ff.text())>0:
                         text_list.append( '[' + unicode(ff.text()) + ']' )
                     else:
@@ -257,11 +263,11 @@ class AppForm(QMainWindow):
 
                 legend = []
                 zlist = [] # list of results
-                for tt in text_list:
+                for [ti,tt] in enumerate(text_list):
                     if not(tt==[]):
                         z = np.array( eval(tt) )
                         zlist.append(y)                        
-                        self.axes.scatter(z[0,], z[1,], marker='x', s=10)
+                        self.axes.scatter(z[0,], z[1,], marker='x', s=10, c=plot_colors[ti])
                         corr = np.corrcoef(z[0,], z[1,])
                         legend.append( "%s %.3f" % (tt, corr[0,1]) )
 
@@ -269,9 +275,7 @@ class AppForm(QMainWindow):
                 min_y, max_y = np.min(z[1,]), np.max(z[1,])
                     
                 self.axes.set_xlim(min_x, max_x)
-                self.axes.set_ylim(min_y, max_y)
-
-                print corr
+                self.axes.set_ylim(min_y, max_y)                
 
                 self.axes.legend(legend, loc=2, prop={'size': 6})
     
@@ -283,17 +287,33 @@ class AppForm(QMainWindow):
         """ Add text box (for function plotting) to figure
         """
         print "Adding"
-        if not(hasattr(self, 'functext')):
-            self.functext = [] # initialise list
 
-        n = len(self.functext)
-
-        self.functext.append(QLineEdit()) # textbox for functions to be executed
-        self.functext[n-1].setMinimumWidth(200)
-        self.create_main_frame()
-        self.on_draw()
-        self.connect(self.functext[n-1], SIGNAL('editingFinished ()'), self.on_draw)
+        if self.display_option==1: # Regression
         
+            if not(hasattr(self, 'functext')):
+                self.functext = [] # initialise list
+
+            n = len(self.functext)
+
+            self.functext.append(QLineEdit()) # textbox for functions to be executed
+            self.functext[n].setMinimumWidth(200)
+            self.create_main_frame()
+            self.on_draw()
+            self.connect(self.functext[n], SIGNAL('editingFinished ()'), self.on_draw)
+        
+        elif self.display_option == 2: # Correlation
+            
+            if not(hasattr(self, 'corrtext')):
+                    self.corrtext = [] # initialise list
+
+            n = len(self.corrtext)
+
+            self.corrtext.append(QLineEdit()) # textbox for functions to be executed
+            self.corrtext[n].setMinimumWidth(200)
+            self.create_main_frame()
+            self.on_draw()
+            self.connect(self.corrtext[n], SIGNAL('editingFinished ()'), self.on_draw)
+
 
 
     def create_main_frame(self):
@@ -303,13 +323,14 @@ class AppForm(QMainWindow):
         # 5x4 inches, 100 dots-per-inch
         #
         self.dpi = 200
+       
         self.fig = Figure((5.0, 4.0), dpi=self.dpi)
         self.canvas = FigureCanvas(self.fig)
         self.canvas.setParent(self.main_frame)
 
         ## new from https://github.com/matplotlib/matplotlib/issues/707/
         self.canvas.setFocusPolicy( Qt.ClickFocus )
-        self.canvas.setFocus()
+        self.canvas.setFocus()        
 
                 
         # Since we have only one plot, we can use add_axes 
@@ -322,7 +343,7 @@ class AppForm(QMainWindow):
             self.axes.set_xlim([-1, 1])
             self.axes.set_ylim([-1, 1])
             self.axes.grid(True, 'major')
-            self.axes.tick_params(axis='both', which='major', labelsize=6)
+            self.axes.tick_params(axis='both', which='major', labelsize=6)            
 
         elif self.display_option==1: # only for regression
             self.axes = self.fig.add_subplot(211)
@@ -393,27 +414,45 @@ class AppForm(QMainWindow):
             
         # Create layout within canvas
 
-        hbox = QHBoxLayout()
-        hbox2 = QHBoxLayout()
+        hbox = QHBoxLayout()   # buttons, sliders, etc.
+        hbox2 = QHBoxLayout()  # text boxes for regression
+        hbox3 = QHBoxLayout()  # text boxes for correlation
         
         # for w in [  self.textbox, self.draw_button, self.grid_cb,
         #             slider_label, self.slider]:
         attr_list = [self.textbox_lim, self.draw_button, self.add_button,
-                        self.slider_label, self.slider_scale, self.comboBox]
-        # if hasattr(self, 'functext'):
-        #     for ff in self.functext:
-        #         attr_list.append(ff)
+                        self.slider_label, self.slider_scale, self.comboBox]        
 
         for w in attr_list:
             hbox.addWidget(w)
             hbox.setAlignment(w, Qt.AlignVCenter)
 
-        if hasattr(self, 'functext'):
+        # initialise or add text boxes       
+        keep_dispopt = self.display_option
+        self.display_option==1 # Regression
+        if not(hasattr(self, 'functext')):
+            self.functext = []
+            self.functext.append(QLineEdit()) # textbox for functions to be executed
+            self.functext[0].setMinimumWidth(200)            
+            self.connect(self.functext[0], SIGNAL('editingFinished ()'), self.on_draw)
+        else:
             for w in self.functext:
                 hbox2.addWidget(w)
                 hbox2.setAlignment(w, Qt.AlignVCenter)
 
-        
+        self.display_option==2 # Correlation
+        if not(hasattr(self, 'corrtext')):
+            self.corrtext = []
+            self.corrtext.append(QLineEdit()) # textbox for functions to be executed
+            self.corrtext[0].setMinimumWidth(200)            
+            self.connect(self.corrtext[0], SIGNAL('editingFinished ()'), self.on_draw)
+        else:
+            for w in self.corrtext:
+                hbox3.addWidget(w)
+                hbox3.setAlignment(w, Qt.AlignVCenter)
+
+        self.display_option = keep_dispopt
+                
         vbox = QVBoxLayout()
         vbox.addWidget(self.canvas)
         # if self.display_option==1: # only for regression
@@ -421,6 +460,7 @@ class AppForm(QMainWindow):
         vbox.addWidget(self.mpl_toolbar)
         vbox.addLayout(hbox)
         vbox.addLayout(hbox2)
+        vbox.addLayout(hbox3)
         
         self.main_frame.setLayout(vbox)
         self.setCentralWidget(self.main_frame)
